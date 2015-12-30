2cfffc7576331547e2d5d3f271d14cb0156b9a7d
James Swineson <jamesswineson@gmail.com>
2015-12-30 20:43:41 +0800

    Pre-compiled headers and common codes used everywhere

---
+++ JStudentManager/common.h
@@ -0,0 +1,16 @@
+//
+//  common.h
+//  JStudentManager
+//
+//  Created by James Swineson on 12/30/15.
+//  Copyright © 2015 James Swineson. All rights reserved.
+//
+
+#ifndef common_h
+#define common_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#endif /* common_h */
